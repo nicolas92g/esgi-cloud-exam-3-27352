ad8792746974f0fc903a0038a212e0268ce0d575
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2024-04-02 12:47:48 +0200

    wasabi connexion

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: '6EN6UBG308XB0TPRU59R', 
+    secretAccessKey: 'mBv5rVVaiTrwLQbsfroE6vZUh7VROVKfHqlejldn',
+    endpoint: 's3.eu-west-2.wasabisys.com', 
+    bucket: 'esgi-cloud-exam-bucket-27352', 
   },
   suffix: {
     small: '_small.jpg',
